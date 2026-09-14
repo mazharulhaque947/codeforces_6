6a066adc4b26354a2d769e603a17677c5dab6d4d
mazharulhaque947 <mazharulhaque94721@gmail.com>
2026-09-14 22:46:19 +0600

    14 sep 2026

--- cf_EhAb AnD gCd.cpp
+++ cf_EhAb AnD gCd.cpp
@@ -1,1 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+
+{
+    int y,x;
+
+    cin>>y;
+
+    while(y--)
+    {
+
+
+        cin>>x;
+
+        cout<<1<<" "<<x-1<<"\n";
+
+    }
+
+    return 0;
+}
